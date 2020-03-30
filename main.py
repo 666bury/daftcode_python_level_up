ec7ce18e198a616ec521de37f75012ff42eb957c
Marcin <buryj666@gmail.com>
2020-03-30 19:17:58 +0200

    next deploy

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 @app.get("/")
 def hello_wrold():
     return {"message": "Hello World"}
+
+class HelloNameResp(BaseModel):
+    message: str
+
+@app.get('/hello/{name}', response_model=HelloNameResp)
+def hello_name(name: str):
+    return HelloNameResp(message=f'Hello {name}')
