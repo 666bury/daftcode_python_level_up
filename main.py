a5ceb802c4d505ae280ff23b59e4ced23ca1dc95
Marcin <buryj666@gmail.com>
2020-04-06 12:19:12 +0200

    deploy second homework

--- main.py
+++ main.py
@@ -10,6 +10,25 @@ def hello():
 class HelloNameResp(BaseModel):
     message: str
 
-@app.get('/hello/{name}', response_model=HelloNameResp)
-def hello_name(name: str):
-    return HelloNameResp(message=f'Hello {name}')
+# @app.get('/hello/{name}', response_model=HelloNameResp)
+# def hello_name(name: str):
+#     return HelloNameResp(message=f'Hello {name}')
+
+@app.get('/method')
+def method():
+    return {"method": "GET"}
+
+
+@app.post('/method')
+def method():
+    return {"method": "POST"}
+
+
+@app.put('/method')
+def method():
+    return {"method": "PUT"}
+
+
+@app.delete('/method')
+def method():
+    return {"method": "DELETE"}
--- test_main.py
+++ test_main.py
@@ -12,9 +12,15 @@ def test_hello():
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World during the coronavirus pandemic!"}
 
-
-@pytest.mark.parametrize("name", ['Zenek', 'Marek', 'Alojzy Niezdąży'])
-def test_hello_name(name):
-    response = client.get(f'/hello/{name}')
+# @pytest.mark.parametrize('method', ['get', 'post', 'put', 'delete'])
+def test_method():
+    response = client.get('/method')
     assert response.status_code == 200
-    assert response.json() == {'message': f'Hello {name}'}
+    assert response.json() == {"method": "GET"}
+
+
+# @pytest.mark.parametrize("name", ['Zenek', 'Marek', 'Alojzy Niezdąży'])
+# def test_hello_name(name):
+#     response = client.get(f'/hello/{name}')
+#     assert response.status_code == 200
+#     assert response.json() == {'message': f'Hello {name}'}
